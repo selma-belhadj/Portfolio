9f69a099f1e3e6fa4ac847bc72fe2fe89c192576
salam <es_belhadj@esi.dz>
2021-12-13 16:01:02 +0100

    add mobile menu

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#menuicon").addEventListener("click", ()=> {
+    document.querySelector(".hamburger").style.display="flex";
+    document.body.style.overflow="hidden"
+
+})
+
+document.querySelector(".closemenu").addEventListener("click", ()=> {
+    document.querySelector(".hamburger").style.display="none";
+    document.body.style.overflow="auto"
+
+})
