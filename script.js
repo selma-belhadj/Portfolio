91b6fccf99f8b4cb9ecc9ba57d31ab344b005e29
mrparkersson <amoparkerprince78@gmail.com>
2021-12-13 15:05:33 +0000

    Add style to overflow

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.querySelector("#menuicon").addEventListener("click", ()=> {
-    document.querySelector(".hamburger").style.display="flex";
-    document.body.style.overflow="hidden"
+document.querySelector("#menuicon").addEventListener("click", () => {
+  document.querySelector(".hamburger").style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
 
-})
-
-document.querySelector(".closemenu").addEventListener("click", ()=> {
-    document.querySelector(".hamburger").style.display="none";
-    document.body.style.overflow="auto"
-
-})
+document.querySelector(".closemenu").addEventListener("click", () => {
+  document.querySelector(".hamburger").style.display = "none";
+  document.body.style.overflow = "auto";
+});
