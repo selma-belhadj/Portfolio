5efe889e4c2e1da7624ae43e41eed1cf5ead7975
salam <es_belhadj@esi.dz>
2021-12-14 15:27:11 +0100

    add mobile menu

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('#menuicon').addEventListener('click', () => {
+    document.querySelector('.hamburger').style.display = 'flex';
+  });
+  
+  document.querySelector('.closemenu').addEventListener('click', () => {
+    document.querySelector('.hamburger').style.display = 'none';
+    // document.body.style.overflow = 'auto';
+  });
+  
+  document.querySelectorAll('#whitebox a').forEach((element) => {
+    element.addEventListener('click', () => {
+      document.querySelector('.hamburger').style.display = 'none';
+      // document.body.style.overflow = 'hidden';
+    });
+  });
+  window.onresize = () => {
+    if (window.innerWidth > 992) {
+      document.querySelector('.hamburger').style.display = 'none';
+    }
+  };
