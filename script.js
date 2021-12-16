510437fd5776d54f8c7aeb22cb2b83f8af317d7e
salam <es_belhadj@esi.dz>
2021-12-16 14:53:04 +0100

    fix validate conatct form errors

--- script.js
+++ script.js
@@ -148,17 +148,29 @@ close.addEventListener('click', () => {
 
 // validate email
 const form = document.querySelector('#form-section form');
+const { email } = form.elements;
+
+function showMsg(input, msg) {
+  const small = document.createElement('small');
+  small.classList.add('error');
+  small.textContent = msg;
+  form.insertBefore(small, input);
+}
+
+function resetMsg() {
+  document.querySelectorAll('small').forEach((s) => s.remove());
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const email = document.getElementById('email');
-  if (email.value.toLowerCase() !== email.value) {
-    const span = document.getElementById('email');
-    span.innerText = 'Your email is not valid, it should be lowercase';
-    span.style.color = 'red';
-    console.log(span)
+
+  resetMsg();
+
+  if (email.value !== email.value.toLowerCase()) {
+    showMsg(email, 'Your email is not valid, it should be lowercase');
     return false;
   }
+
   form.submit();
   return true;
 });
