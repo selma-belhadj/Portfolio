7553168198bd42d8a1cbf2ff1c1b1f466e40a1c2
salam <es_belhadj@esi.dz>
2021-12-16 16:38:50 +0100

    add preserving data in the browser

--- script.js
+++ script.js
@@ -145,32 +145,3 @@ document.querySelectorAll('.work-btn').forEach((btn) => {
 close.addEventListener('click', () => {
   workDetail.style.display = 'none';
 });
-
-// validate email
-const form = document.querySelector('#form-section form');
-const { email } = form.elements;
-
-function showMsg(input, msg) {
-  const small = document.createElement('small');
-  small.classList.add('error');
-  small.textContent = msg;
-  form.insertBefore(small, input);
-}
-
-function resetMsg() {
-  document.querySelectorAll('small').forEach((s) => s.remove());
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  resetMsg();
-
-  if (email.value !== email.value.toLowerCase()) {
-    showMsg(email, 'Your email is not valid, it should be lowercase');
-    return false;
-  }
-
-  form.submit();
-  return true;
-});
